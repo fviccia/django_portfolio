480114a4b17b4c48a98206dbc9756370777d4e58
Franco Viccia <franco.viccia@avature.net>
2022-01-19 21:44:03 -0300

    Changes on project

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,12 @@
+# Here is where the logic of the code happens
+
+
 from django.http import HttpResponse
 from django.shortcuts import render
+from projects.models import Project
 
-# Create your views here.
 
-
-def project_list(request):
-    return render(request, 'projects/index.html')
+def all_projects(request):
+    # Query the database to return all the projects objects
+    projects = Project.objects.all()
+    return render(request, 'projects/all_projects.html', {'projects': projects})
